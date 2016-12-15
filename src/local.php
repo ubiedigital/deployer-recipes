2385502689e672195707a59a8594530a13434223
egidijus <egidijus>
2016-12-15 15:40:27 +0200

    updated path

--- src/local.php
+++ src/local.php
@@ -2,7 +2,7 @@
 
 namespace Deployer;
 
-require_once __DIR__.'/../vendor/deployer/recipes/local.php';
+require_once 'vendor/deployer/recipes/local.php';
 
 set('local/bin/php', function () {
     return runLocally('which php')->toString();
