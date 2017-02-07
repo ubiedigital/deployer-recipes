69ff2b0ae510d391c50c2a4cf451350e7bd9fe77
egidijus <egidijus>
2017-02-07 13:59:45 +0200

    add task property to gulp build command

--- src/local_gulp.php
+++ src/local_gulp.php
@@ -2,6 +2,8 @@
 
 namespace Deployer;
 
+set('gulp_task', 'build');
+
 task('local:gulp:build', function() {
-    runLocally('cd {{local_release_path}} && gulp build');
+    runLocally('cd {{local_release_path}} && gulp {{gulp_task}}');
 })->desc('Gulp build');
