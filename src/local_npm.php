01bdabc4dec789ffca7e7aee6f411f18210c7471
egidijus <egidijus>
2017-02-08 16:56:17 +0200

    npm-cache:install task arguments

--- src/local_npm.php
+++ src/local_npm.php
@@ -6,6 +6,8 @@ task('local:npm:install', function () {
     runLocally('cd {{local_release_path}} && {{local_npm_path}} install --cache-min');
 })->desc('NPM install');
 
+set('npm_cache_arguments', '');
+
 task('local:npm-cache:install', function () {
-    runLocally('cd {{local_release_path}} && {{local_npm_cache_path}} install');
+    runLocally('cd {{local_release_path}} && {{local_npm_cache_path}} install {{npm_cache_arguments}}');
 })->desc('NPM install');
