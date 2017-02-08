4eb293b26ecc5aa8b164f20b6f4121ffd8c4edd9
egidijus <egidijus>
2017-02-08 16:43:31 +0200

    npm-cache install task

--- src/local_npm.php
+++ src/local_npm.php
@@ -5,3 +5,7 @@ namespace Deployer;
 task('local:npm:install', function () {
     runLocally('cd {{local_release_path}} && {{local_npm_path}} install --cache-min');
 })->desc('NPM install');
+
+task('local:npm-cache:install', function () {
+    runLocally('cd {{local_release_path}} && {{local_npm_cache_path}}');
+})->desc('NPM install');
