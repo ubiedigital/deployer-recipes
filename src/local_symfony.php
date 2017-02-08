62320a9ceaf485251538c4b17786da08cbb76398
egidijus <egidijus>
2017-02-08 11:04:01 +0200

    fos:js-routing:dump local task

--- src/local_symfony.php
+++ src/local_symfony.php
@@ -9,6 +9,10 @@ set('local/bin/console', function () {
     return sprintf('{{local_release_path}}/%s/console', trim(get('bin_dir'), '/'));
 });
 
+task('local:js-routing:dump', function () {
+    runLocally('{{env_vars}} {{local/bin/php}} {{local/bin/console}} fos:js-routing:dump {{console_options}}');
+})->desc('Install bundle assets');
+
 task('local:assets:install', function () {
     runLocally('{{env_vars}} {{local/bin/php}} {{local/bin/console}} assets:install {{console_options}} {{local_release_path}}/web');
 })->desc('Install bundle assets');
